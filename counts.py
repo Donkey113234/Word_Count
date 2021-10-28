b07582a0e32bf2796261a8752ed45502b957110f
Priyansh Saxena <31817458+deathzombie@users.noreply.github.com>
2021-10-28 20:50:50 +0530

    Update counts.py

--- counts.py
+++ counts.py
@@ -1,3 +1,5 @@
-#program to count the letters(including spaces)
-x = input("Enter a sentence:")
-print(len(x))
+#program to count the words use in a sentence/string
+string = input("Enter a sentence : ")
+a = string.split()
+
+print(len(a))
